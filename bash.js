afa46c43433e45ffdc0f180f97965c131d71d01b
Masaki Shibano <masakishibano@gmail.com>
2022-03-04 13:01:56 -0800

    completed bonus: implementing cat

--- bash.js
+++ bash.js
@@ -1,20 +1,23 @@
 // Node.js program to demonstrate the
 // process.cwd() Method
-const pwd = require("./pwd")
-const ls = require("./ls")
+const pwd = require("./pwd");
+const ls = require("./ls");
+const cat = require("./cat");
 // output a prompt
 process.stdout.write("prompt > ");
 
 // the stdin 'data' event fires after a user types in a line
 process.stdin.on("data", (data) => {
-  const cmd = data.toString().trim(); // remove the newline
-  
+  let cmd = data.toString().trim().split(" "); // remove the newline
+
   if (cmd === "pwd") {
-    pwd
-  } else if (cmd === "ls"){
-    ls()
-  }
-   else {
+    pwd;
+  } else if (cmd[0] === "ls") {
+    ls();
+  } else if (cmd[0] === "cat") {
+    cat(cmd[1]);
+  } else {
+    cmd = cmd.join(" ");
     process.stdout.write("you typed: " + cmd);
     process.stdout.write("\nprompt > ");
   }
--- cat.js
+++ cat.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 
-module.exports = function () {
-  fs.readdir("./", "utf8", (err, files) => {
+module.exports = function (cmd) {
+  fs.readFile(`./${cmd}.js`, "utf8", (err, files) => {
     if (err) {
       throw err;
     } else {
-      process.stdout.write(files.join("\n"));
+      process.stdout.write(files);
       process.stdout.write("\nprompt > ");
     }
   });
